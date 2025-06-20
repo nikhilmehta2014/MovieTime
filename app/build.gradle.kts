f86b051930166c95484e173a44ba35f128005264
Nikhil <nikhilmht33@gmail.com>
2025-06-20 20:28:49 +0530

    Add TMBD API Key to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "TMDB_API_KEY",
+            "\"${project.findProperty("TMDB_API_KEY") ?: ""}\""
+        )
     }
 
     buildTypes {
